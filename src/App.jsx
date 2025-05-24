4cd74331474a0e47234984ff12ca3ff3908a2044
Ana Caroline <85416744+anacarolinens@users.noreply.github.com>
2025-05-24 00:00:51 -0500

    ✨ feat: corrige nome skills no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Hero />
         <About />
         <ExperienceList />
-        <Skils />
+        <Skills />
         <Projects />
         {/* <Contact /> */}
       </main>
